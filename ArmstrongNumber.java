4bc6210e43f21b1545912e6012ab869ee657c12b
vishranti28 <vishrantikoli28@gmail.com>
2025-05-28 01:44:42 +0900

    Add files via upload

---
+++ ArmstrongNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ArmstrongNumber {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number");
+        } else {
+            System.out.println(number + " is not an Armstrong number");
+        }
+        scanner.close();
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNum = num;
+        int sum = 0;
+        int digits = String.valueOf(num).length();
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        return sum == originalNum;
+    }
+}
